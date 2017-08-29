e778e4db0f8949663ed36dac99f091774a4373d2
Frank Paulo Filho <me@frankpf.com>
2017-08-29 20:35:56 -0300

    Remove manual printing from `rust_main`

--- src/lib.rs
+++ src/lib.rs
@@ -11,31 +11,6 @@ fn fill_row(start: u32, row_num: u32) -> () {
 
 #[no_mangle]
 pub extern fn rust_main() {
-    /*
-    let hello = b"Hello world!";
-    let color_byte = 0x1f;
-
-    let rows = 25;
-    let cols = 80;
-
-    let mut hello_colored = [color_byte; 24];
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colored[i*2] = *char_byte;
-    }
-
-    // Write "Hello world" to the center
-    // of the VGA text buffer
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colored };
-    */
-    let start = 0xb8000;
-
-    let mut column = 0;
-
-    while column < 80 {
-        fill_row(start, column);
-        column += 1;
-    }
 
     loop{}
 }
