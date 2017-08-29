6356ae2e211d5ac19515c1e835f5665061d3d6dd
Frank Paulo Filho <me@frankpf.com>
2017-08-29 20:36:46 -0300

    Add driver to control the VGA buffer

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 #![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(unique)]
 #![no_std]
+
+extern crate spin;
+extern crate volatile;
 extern crate rlibc;
 
-fn fill_row(start: u32, row_num: u32) -> () {
-    let buffer_ptr = (start + row_num*80) as *mut _;
-    let color_byte = 0x1f201f20;
-    let line = [color_byte; 40];
-    unsafe { *buffer_ptr = line };
-}
+#[macro_use]
+mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
+    vga_buffer::clear_screen();
 
     loop{}
 }
